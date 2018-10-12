1879a272e0677b3cca5007c5d138c1822f6b5ede
gruggiero <giovanni.ruggiero@gmail.com>
2018-10-12 13:22:52 +0200

    Added deploy

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+# deployEnv=$1
+
+kubectl --kubeconfig=$KUBECONFIG delete configmap daf-configurator-conf -n testci || true
+kubectl --kubeconfig=$KUBECONFIG create configmap daf-configurator-conf -n testci --from-file=../conf/${DEPLOY_ENV}/prodBase.conf
+kubectl --kubeconfig=$KUBECONFIG replace -f daf_configurator_${DEPLOY_ENV}.yml -n testci --force
