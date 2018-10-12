9b5e2e15e978a1cb5a1bd0896972411714b76dcd
gruggiero <giovanni.ruggiero@gmail.com>
2018-10-12 13:42:28 +0200

    Added logback

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,4 +6,5 @@ set -e
 
 kubectl --kubeconfig=$KUBECONFIG delete configmap daf-configurator-conf -n testci || true
 kubectl --kubeconfig=$KUBECONFIG create configmap daf-configurator-conf -n testci --from-file=../conf/${DEPLOY_ENV}/prodBase.conf
+kubectl --kubeconfig=$KUBECONFIG replace -f daf_configurator_logback.yml -n testci --force
 kubectl --kubeconfig=$KUBECONFIG replace -f daf_configurator_${DEPLOY_ENV}.yaml -n testci --force
